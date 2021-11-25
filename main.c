9085502ac06a736f2f20aaa8ed5ca100de9a5d40
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2021-11-25 09:19:21 +0100

    *PROJET SAS*

--- main.c
+++ main.c
@@ -15,11 +15,11 @@ typedef struct compteBancaire
 void classerCroix(compteBancaire T[], int taille);
 void trierParSelection(compteBancaire T[], int taille, char type[4], int chiffre);
 void ajouterFidelisation(compteBancaire T[], int taille);
-void neew(char *menu);
+void neew(char menu[]);
 int main()
 {
 
-    int i = 0, j = 0, tailleCB = 0;sss
+    int i = 0, j = 0, tailleCB = 0;
     int choix = 0;
     int anyKey;//just to stop the process and press any key to complete
     int addFidelisation = 0;//check if i added fidelisation 0 means not yet
@@ -30,18 +30,18 @@ int main()
     int numOfAcc = 0;//add 1 account or many
     char searchCIN[10];//var for storing the scanf of CIN
     char firstNames[15][30] = {"Imane", "Souhaila", "MedAmine", "Walid", "Faiza", "Hakim", "Abdullah", "Ilyas", "Manal", "Aya", "Khadija", "Meriem", "Ibtissam", "Hiba", "Hasnae"};
-    char lastNames[15][30] = {"MOUltamiss", "Stayli", "Garrix", "Doufare", "Joumar", "Belbhiriya", "AitGaba", "Ali", "Lechgar", "Mesbahi", "Siraj", "Daalabi", "ElBoustani", "Grich", "Belbaraka"};
+    char lastNames[15][30] = {"Moultamiss", "Stayli", "Garrix", "Doufare", "Joumar", "Belbhiriya", "AitGaba", "Ali", "Lechgar", "Mesbahi", "Siraj", "Daalabi", "ElBoustani", "Grich", "Belbaraka"};
     char arrCIN[15][30] = {"AAAA", "BBBB", "CCCC", "DDDD", "EEEE", "FFFF", "JJJJ", "HHHH", "KKKK", "IIII", "MMMM", "NNNN", "OOOO", "PPPP", "QQQQ"};
     float arrMon[15] = {10000, 24000, 3000, 2000, 10000, 40000, 5800, 3490, 23456, 19870, 12987, 19990, 23000, 30000, 45600};
 
-    compteBancaire compteBancaires[40];//create table of compteBancaires based on struct contain taillebc of elements
+    compteBancaire compteBancaires[40];//create table of compteBancaires based on struct
 
-Menu:
+    Menu:
 
-    do
+      do
     {
         system("cls");
-        neew("\t\t;BIENVENUE DANS VOTRE MENU");
+        printf("\t\t*****BIENVENUE DANS VOTRE MENU*****");
         printf("\n\n");
         printf("\n 1: Ajouter compte\n");
         printf("\n 2: Gestion compte\n");
@@ -60,21 +60,24 @@ Menu:
             break;
         case 3:
             printf("Au revoir");
+            goto Menu;
             choix = 0;
             break;
         default:
             printf("Choix Inattendu! Entrer une autre fois");
+            Sleep(2000);
+            goto Menu;
             choix = 0;
             break;
         }
     } while (choix > 3);
 
-MenuAddAcc:
+    MenuAddAcc:
 
-    do
-    {
+        do
+         {
         system("cls");
-        printf("\t\t\tMenu ajouter compte**\n");
+        printf("\t\t***Menu Ajouter Compte***\n");
         printf("\n1: Ajouter un seul compte\n");
         printf("\n2: Ajouter Plusieurs comptes\n");
         printf("\n3: Ajouter Plusieurs comptes (Auto 15 comptes)\n");
@@ -100,6 +103,7 @@ MenuAddAcc:
             break;
         default:
             printf("Choix Inattendu! Entrer Une Autre Fois");
+            Sleep(2000);
             break;
         }
     } while (choix > 3);
@@ -109,7 +113,7 @@ MenuSearchAcc:
     do
     {
         system("cls");
-        printf("\t\t\t*Menu Rechercher compte*\n");
+        printf("\t\t***Menu Rechercher Compte***\n");
         printf("\n1: Rechercher un compte\n");
         printf("\n2: Afficher tout les comptes\n");
         printf("\n3: Ajoutez fidelisation 1.3%% pour les 3 premiers\n");
@@ -160,19 +164,21 @@ goto MenuSearchAcc;
 
 
 AffTtAcc:
+    system("cls");
     do
     {
-       neew("Tous les comptes sont statistiques");
+       printf("\t\t*****statistiques des comptes*****\n ");
 
         //affichage de tous les compte
         for (int i = 0; i < nombre_compte; i++)
         {
-            printf("%d: %s %s \t %s \t %.2lf\n", i + 1, compteBancaires[i].nom, compteBancaires[i].prenom, compteBancaires[i].CIN, compteBancaires[i].montant);
+            //Sleep(2000);
+            printf("%d: %s %s \t %s \t %f\n", i + 1, compteBancaires[i].nom, compteBancaires[i].prenom, compteBancaires[i].CIN, compteBancaires[i].montant);
         }
 
         do
         {
-            printf("\t\t**Trier par**\n");
+            printf("\t\t****Trier par****\n");
             printf("\n1: ASC\n");
             printf("\n2: DESC\n");
             printf("\n3: ASC avec limite\n");
@@ -184,23 +190,23 @@ AffTtAcc:
             {
             case 1:
                 system("cls");
-                trierParSelection(compteBancaires, tailleCB, "ASC", 0);
+                trierParSelection(compteBancaires, nombre_compte, "ASC", 0);
                 break;
             case 2:
                 system("cls");
-                trierParSelection(compteBancaires, tailleCB, "DESC", 0);
+                trierParSelection(compteBancaires, nombre_compte, "DESC", 0);
                 break;
             case 3:
                 printf("Entrer un montant:");
                 scanf("%d", &chiffre);
                 system("cls");
-                trierParSelection(compteBancaires, tailleCB, "ASC", chiffre);
+                trierParSelection(compteBancaires, nombre_compte, "ASC", chiffre);
                 break;
             case 4:
                 printf("Entrer un montant:");
                 scanf("%d", &chiffre);
                 system("cls");
-                trierParSelection(compteBancaires, tailleCB, "DESC", chiffre);
+                trierParSelection(compteBancaires, nombre_compte, "DESC", chiffre);
                 break;
             case 5:
                 printf("Au revoir");
@@ -222,7 +228,7 @@ AffTtAcc:
 
 search1Compte:
 
-    neew("Rechercher un compte avec CIN");
+    printf("\t\t***Rechercher un compte avec CIN***\n");
     printf("Entrer la CIN:");
     scanf("%s", searchCIN);
 
@@ -231,13 +237,13 @@ search1Compte:
         if (strcmp(searchCIN, compteBancaires[i].CIN) == 0)
         {
             iOfChoosedAcc =i;//pour memoiriser le I de compte banquaire pour fair le retrait ou le depot
-            neew("menu");
-            printf("\n\n\n%d: %s %s \t %s \t %.2lf\n", i + 1, compteBancaires[i].nom, compteBancaires[i].prenom, compteBancaires[i].CIN, compteBancaires[i].montant);
+            printf("\t\t***menu***\n");
+            printf("\n\n\n%d: %s %s \t %s \t %f\n", i + 1, compteBancaires[i].nom, compteBancaires[i].prenom, compteBancaires[i].CIN, compteBancaires[i].montant);
 
             do
             {
                 system("cls");
-                printf("\t\t*Operations*\n");
+                printf("\t\t***OPERATIONS***\n");
                 printf("\n1: RETRAIT\n");
                 printf("\n2: DEPOT\n");
                 printf("\n3: Retour\n");
@@ -277,7 +283,7 @@ search1Compte:
 
 retrait:
     system("cls");
-    printf("Retrait de Mr %s qui a %.2f", compteBancaires[iOfChoosedAcc].prenom, compteBancaires[iOfChoosedAcc].montant);
+    printf("Retrait de Mr %s qui a %f", compteBancaires[iOfChoosedAcc].prenom, compteBancaires[iOfChoosedAcc].montant);
     printf("Entrer le montant a retirer:");
     scanf("%f", &montantARetrait);
     if (compteBancaires[iOfChoosedAcc].montant >= montantARetrait)
@@ -377,12 +383,12 @@ addAccAuto:
     return 0;
 }
 }
-void neew (char *menu)
+/*void neew (char menu[])
 {
-    system("cls");
-    printf("****%s*****\n\n\n", menu);
+    //system("cls");
+    printf("\t\t\t*****%s*****\n\n\n", menu);
 }
-
+*/
 void classerCroix(compteBancaire T[], int taille)
 {
     int i = 0;
@@ -425,7 +431,7 @@ void trierParSelection(compteBancaire T[], int taille, char type[4], int chiffre
     {
         if (T[i].montant > chiffre)
         {
-            printf("\n%d: %s %s \t %s \t %.2lf", i + 1, T[i].nom, T[i].prenom, T[i].CIN, T[i].montant);
+            printf("\n%d: %s %s \t %s \t %f", i + 1, T[i].nom, T[i].prenom, T[i].CIN, T[i].montant);
         }
     }
 }
